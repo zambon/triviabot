77af4cb0ae55fc3b8f604cdb3b89a040dc5cd505
Henrique Zambon <henrique.zambon@gmail.com>
2015-02-01 02:21:57 -0500

    Award points for correct answers

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -11,12 +11,17 @@ module.exports = function (robot) {
 
   var currentTrivia;
   var gameOn = false;
+  var started = false;
 
   robot.respond(/h[ae]+lp/i, function (msg) {
     msg.send("halp: help");
   });
 
   robot.respond(/start game/i, function(msg) {
+    if (started) return;
+
+    started = true;
+
     gameLoop(msg);
     setInterval(function() {
       gameLoop(msg);
@@ -33,6 +38,7 @@ module.exports = function (robot) {
       gameOn = false;
 
       msg.send("Answer is correct! Congratulations " + user.name + ".");
+      MyCloudant.userScored(user);
     }
   });
 
