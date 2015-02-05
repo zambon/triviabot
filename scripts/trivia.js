0be94ef4bc22e5a250cc96412f74b172121155e1
Henrique Zambon <henrique.zambon@gmail.com>
2015-02-04 20:01:46 -0500

    - Use Levenshtein distance to determine correct answers - Remove deprecated method `randomQuestion` from MyCloudant lib - Add "close enough" messages for imprecise answers

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -1,11 +1,12 @@
-var cfenv = require("cfenv");
-var S     = require("string");
+var cfenv       = require("cfenv");
+var S           = require("string");
+var levenshtein = require("fast-levenshtein");
 
 var MyCloudant = require("../lib/my_cloudant");
 var jService   = require("../lib/jservice");
 var phrases    = require("../lib/talkshow_catch_phrases_generator");
 
-var appEnv     = cfenv.getAppEnv();
+var appEnv = cfenv.getAppEnv();
 
 MyCloudant = new MyCloudant(
   appEnv.getServiceCreds(/cloudant/i).username,
@@ -23,24 +24,34 @@ module.exports = function(robot) {
   var answerTimer;
 
   //
+  // Greetings
+  //
+  enterReplies = ["Hi", "Target Acquired", "Firing", "Hello friend.", "Gotcha", "I see you", "Hello", "Howdy"]
+  robot.enter(function(msg) {
+    msg.send(msg.random(enterReplies));
+  });
+
+  //
   // Commands
   //
   robot.respond(/h[ae]+lp/i, function(msg) {
     msg.send("*halp*: This help menu");
     msg.send("*start game*: Start a new trivia game");
     msg.send("*stop game*: Stop the current trivia game");
-    msg.send("*leaderboard*: Show top ten players");
+    msg.send("*leaderboard*: Show top ten players and their scores");
     msg.send("*my score*: Show your current score");
   });
 
+  // Start a new trivia game
   robot.respond(/start game/i, function(msg) {
-    if (started) return;
+    if(started) return;
     started = true;
     startGame(msg);
   });
 
+  // Stop the current trivia game
   robot.respond(/stop game/i, function(msg) {
-    if (!started) return;
+    if(!started) return;
 
     msg.send("Game over! Use the *start game* command to start a new one!");
 
@@ -52,6 +63,7 @@ module.exports = function(robot) {
     clearTimeout(answerTimer);
   });
 
+  // Show top ten players and their scores
   robot.respond(/leaderboard/i, function(msg) {
     MyCloudant.leaderboard(function(leaderboard) {
 
@@ -64,6 +76,7 @@ module.exports = function(robot) {
     });
   });
 
+  // Show your current score
   robot.respond(/my score/i, function(msg) {
     MyCloudant.userScore(msg.message.user, function(response) {
       msg.send("@" + response._id + " " + response.score);
@@ -74,15 +87,25 @@ module.exports = function(robot) {
   // Answers
   //
   robot.hear(/.+/i, function(msg) {
-    if (!gameOn || !currentTrivia) return;
+    if(!gameOn || !currentTrivia) return;
 
-    var guess = msg.message.text;
-    if (guess.toLowerCase() === S(currentTrivia.answer).stripTags().s.toLowerCase()) {
-      var user = msg.message.user;
+    var guess    = msg.message.text;
+    var answer   = S(currentTrivia.answer).stripTags().s;
+    var distance = levenshtein.get(guess.toLowerCase(), answer.toLowerCase());
 
+    if(distance < 5) {
       gameOn = false;
 
-      msg.send("_" + msg.random(phrases.goodJob("*@"+user.name+"*")) + "_");
+      var user = msg.message.user;
+      var message;
+
+      if(distance == 0) {
+        message = "_" + msg.random(phrases.goodJob("*@" + user.name + "*")) + "_";
+      } else {
+        message = "_" + msg.random(phrases.closeEnough("*@" + user.name + "*")) + " The answer was *" + answer + "*._";
+      }
+
+      msg.send(message);
       MyCloudant.userScored(user);
 
       setTimeout(function() {
@@ -113,7 +136,7 @@ module.exports = function(robot) {
       msg.send("[" + currentTrivia.category.title + "] *" + S(currentTrivia.question).stripTags().s + "*");
 
       answerTimer = setTimeout(function() {
-        if (gameOn) {
+        if(gameOn) {
           msg.send("_*Time's up!* The answer was *" + S(currentTrivia.answer).stripTags().s + "*._");
           gameOn = false;
         }
